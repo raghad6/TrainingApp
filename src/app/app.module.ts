77fed751f7459800ac58238c128ca025304467ca
raghad6 <raghadsharar4@gmail.com>
2022-02-12 09:32:04 -0800

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,10 @@ import { TraineeProfileComponent } from './trainee-profile/trainee-profile.compo
 import { TrainerProfileComponent } from './trainer-profile/trainer-profile.component';
 import { UniProfileComponent } from './uni-profile/uni-profile.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
-import { TraineeHoursCal } from './trainee-hours-cal./trainee-hours-cal..component';
 import { CreateToDoListComponent } from './create-to-do-list/create-to-do-list.component';
 import { UniTraineesListComponent } from './uni-trainees-list/uni-trainees-list.component';
 import { TraineeAccessScreenComponent } from './trainee-access-screen/trainee-access-screen.component';
-import { TrianeeFinishedT } from './trianee-finished-t.h/trianee-finished-t.h.component';
-import { LoginPasswordCreationComponent } from './login-password-creation/login-password-creation.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -23,13 +21,12 @@ import { LoginPasswordCreationComponent } from './login-password-creation/login-
     TrainerProfileComponent,
     UniProfileComponent,
     ToDoListComponent,
-    TraineeHoursCal.Component,
     CreateToDoListComponent,
     UniTraineesListComponent,
     TraineeAccessScreenComponent,
-    TrianeeFinishedT.HComponent,
-    LoginPasswordCreationComponent
-  ],
+    SignupComponent
+    ],
+    
   imports: [
     BrowserModule,
     AppRoutingModule
