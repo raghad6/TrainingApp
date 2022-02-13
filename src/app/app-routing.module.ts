5e178060012bbe8d03e5e6059b62cd11223155ca
Ahmad arafeh <181032@ppu.edu.ps>
2022-02-13 19:44:12 +0200

    Application form , router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplicationFormComponent } from './application-form/application-form.component';
+import { ComingHoursComponent } from './coming-hours/coming-hours.component';
 import { CreateToDoListComponent } from './create-to-do-list/create-to-do-list.component';
 import { LoginPasswordCreationComponent } from './login-password-creation/login-password-creation.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { TraineeAccessScreenComponent } from './trainee-access-screen/trainee-access-screen.component';
-import { TraineeHoursCal } from './trainee-hours-cal./trainee-hours-cal..component';
+// import { TraineeHoursCal } from './trainee-hours-cal./trainee-hours-cal.component';
 import { TraineeProfileComponent } from './trainee-profile/trainee-profile.component';
 import { TrainerProfileComponent } from './trainer-profile/trainer-profile.component';
 import { UniProfileComponent } from './uni-profile/uni-profile.component';
 import { UniTraineesListComponent } from './uni-trainees-list/uni-trainees-list.component';
 
 const routes: Routes = [
-  { path :'login' , component: LoginComponent } , 
+  { path :'login' , component: LoginComponent } ,
   { path: 'signup' , component: LoginPasswordCreationComponent},
+  // { path: 'signup' , component: SignUpComponent},
   { path: 'Create-todolist' , component: CreateToDoListComponent},
   { path: 'ToDolist' , component: ToDoListComponent                },
   { path: 'Trainee-Access' , component: TraineeAccessScreenComponent},
   { path: 'Trainee-profile' , component: TraineeProfileComponent},
   { path: 'Trainer-profile' , component: TrainerProfileComponent},
-  { path: 'Trainee-Hours' , component: TraineeHoursCal},
+  // { path: 'Trainee-Hours' , component: TraineeHoursCal},
   { path: 'Uni-profile' , component: UniProfileComponent},
-  { path: 'Uni-trainees' , component: UniTraineesListComponent}
+  { path: 'Uni-trainees' , component: UniTraineesListComponent},
+  { path: 'App-form' , component: ApplicationFormComponent},
+  { path: 'Coming-hours' , component: ComingHoursComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,15 @@ import { TraineeProfileComponent } from './trainee-profile/trainee-profile.compo
 import { TrainerProfileComponent } from './trainer-profile/trainer-profile.component';
 import { UniProfileComponent } from './uni-profile/uni-profile.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
-import { TraineeHoursCal } from './trainee-hours-cal./trainee-hours-cal..component';
+// import { TraineeHoursCal } from './trainee-hours-cal./trainee-hours-cal..component';
 import { CreateToDoListComponent } from './create-to-do-list/create-to-do-list.component';
 import { UniTraineesListComponent } from './uni-trainees-list/uni-trainees-list.component';
 import { TraineeAccessScreenComponent } from './trainee-access-screen/trainee-access-screen.component';
-import { TrianeeFinishedT } from './trianee-finished-t.h/trianee-finished-t.h.component';
+// import { TrianeeFinishedT } from './trianee-finished-t.h/trianee-finished-t.h.component';
 import { LoginPasswordCreationComponent } from './login-password-creation/login-password-creation.component';
+import { ComingHoursComponent } from './coming-hours/coming-hours.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ApplicationFormComponent } from './application-form/application-form.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +26,15 @@ import { LoginPasswordCreationComponent } from './login-password-creation/login-
     TrainerProfileComponent,
     UniProfileComponent,
     ToDoListComponent,
-    TraineeHoursCal.Component,
+    // TraineeHoursCal.Component,
     CreateToDoListComponent,
     UniTraineesListComponent,
     TraineeAccessScreenComponent,
-    TrianeeFinishedT.HComponent,
-    LoginPasswordCreationComponent
+    // TrianeeFinishedT.HComponent,
+    LoginPasswordCreationComponent,
+    ComingHoursComponent,
+    SignUpComponent,
+    ApplicationFormComponent
   ],
   imports: [
     BrowserModule,
