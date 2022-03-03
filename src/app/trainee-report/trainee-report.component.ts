04df16b709df144bdfcefbb608ab4afeb7c9dca5
LENOVO <nagham.752000@gmail.com>
2022-03-03 13:21:09 +0200

    s

--- src/app/trainee-report/trainee-report.component.ts
+++ src/app/trainee-report/trainee-report.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TraineeReportComponent implements OnInit {
 
+  popup = false
+  name = 'Angular'; 
+
   constructor() { }
 
   ngOnInit(): void {
