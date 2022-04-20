5e32b2fea48190858e05dba31e229003ceb852d9
Ahmad arafeh <181032@ppu.edu.ps>
2022-04-20 05:06:24 +0300

    popup button

---
+++ src/app/trainee-report/trainee-report.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatDialog} from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-trainee-report',
+  templateUrl: './trainee-report.component.html',
+  styleUrls: ['./trainee-report.component.css']
+})
+
+export class TraineeReportComponent implements OnInit {
+  popup = false
+  name = 'Angular';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
+  }
+
+
+  // openDialog() {
+  //   this.dialog.open(this.DialogElementsExampleDialog);
+  // }
+  // export class: any DialogElementsExampleDialog: any {}
+
+
+}
+
