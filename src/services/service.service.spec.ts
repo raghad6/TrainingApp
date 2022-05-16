648c369aff403dae5165704f2ec0f09e2d597ae6
raghad6 <raghadsharar4@gmail.com>
2022-05-16 13:51:05 +0300

    improvments

--- src/services/service.service.spec.ts
+++ src/services/service.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ServiceService } from '../services/service.service';
+import { ServiceService } from './service.service';
 
 describe('ServiceService', () => {
   let service: ServiceService;
