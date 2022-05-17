99ee180c9b0d6096d163fa5482b06bc21d28ff80
LENOVO <nagham.752000@gmail.com>
2022-05-17 22:07:42 +0300

    edits

--- src/pages/signup/signup.component.ts
+++ src/pages/signup/signup.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-// import { ConfirmedValidator } from '../confirmed.validator';
+import { Title } from '@angular/platform-browser';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+
+export class SignUpComponent implements OnInit {
+
+
+  Title = 'Sign Up';
+  signup = new FormControl('vali');
 
   signupForm = new FormGroup({
     email: new FormControl(''),
-    pass1: new FormControl(''),
-    pass2: new FormControl(''),
-
+    pass: new FormControl(''),
   });
   formBuilder: any;
 
@@ -22,29 +25,25 @@ export class SignupComponent implements OnInit {
   constructor(private router: Router ,
     formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-    this.signupForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      pass1: ['', Validators.required],
-      pass2: ['', Validators.required],
+    ngOnInit(): void {
+      this.signupForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        pass: ['', Validators.required],
+    });
 
-  },
-  {
-    // validator: ConfirmedValidator('pass1', 'pass2')
+    }
+
+    get valida() { return this.signupForm.controls; }
+
+
+
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
   }
-  );
+
+  submit(){
+      if (this.signupForm.invalid) { return }
+    }
+    
   }
-
-  get valida() { return this.signupForm.controls; }
-
-goToPage(pageName:string){
-  this.router.navigate([`${pageName}`]);
-}
-
-submit(){
-    if (this.signupForm.invalid) { return  }
-    else   this.router.navigate([`login`]);
-
-  }
-}
   
